75f28ad1d54b72c9748610582b16515539a47228
cellis999 <wconnorellis@gmail.com>
2026-09-17 10:52:08 -0600

    finished js-variables

---
+++ W01/js-variables.js
@@ -0,0 +1,14 @@
+const pi = 3.14;
+let radius = 3;
+
+const one = 1;
+const two = '2';
+
+let course = 'wdd 131';
+if (true) {
+    let student = 'Connor';
+    console.log(student);
+    console.log(course);
+}
+console.log(course);
+console.log(student); // this will throw an error because it is declared in the block scope not global
