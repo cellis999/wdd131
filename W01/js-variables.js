7dd6762faa5f03a607b41ea02feacc8725b62bee
cellis999 <wconnorellis@gmail.com>
2026-09-17 18:20:23 -0600

    finished other ponder

--- W01/js-variables.js
+++ W01/js-variables.js
@@ -9,6 +9,9 @@ if (true) {
     let student = 'Connor';
     console.log(student);
     console.log(course);
+    favoriteColor = 'blue'; // this is a global variable because it is not declared with var, let, or const
 }
 console.log(course);
 console.log(student); // this will throw an error because it is declared in the block scope not global
+
+document.querySelector('h1').style.color = favoriteColor;
